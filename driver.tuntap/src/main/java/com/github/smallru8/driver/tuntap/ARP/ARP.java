53041bc911802ea67f0c021523d55e02a4d8fd06
smallru8 <30855604+smallru8@users.noreply.github.com>
2020-03-08 14:39:40 +0800

    Update ARP.java

--- driver.tuntap/src/main/java/com/github/smallru8/driver/tuntap/ARP/ARP.java
+++ driver.tuntap/src/main/java/com/github/smallru8/driver/tuntap/ARP/ARP.java
@@ -28,7 +28,7 @@ public class ARP extends TimerTask{
 				return it.next().MACAddr;
 			}
 		}
-		return null;
+		return null;//table沒有紀錄
 	}
 	
 	/**
@@ -38,9 +38,64 @@ public class ARP extends TimerTask{
 	 * @param srcMACAddr
 	 * @return
 	 */
-	public byte[] generateARPrequestPacket(int srcIPAddr,byte[] srcMACAddr,int desIPAddr) {//
+	public byte[] generateARPrequestPacket(int srcIPAddr,byte[] srcMACAddr,int desIPAddr) {
+		byte[] packet = new byte[60];
 		
-		return null;
+		//des MAC addr FF:FF:FF:FF:FF:FF
+		for(int i=0;i<6;i++)
+			packet[i] = (byte) 0xFF;
+		
+		//src MAC addr srcMACAddr
+		for(int i=0;i<6;i++)
+			packet[i+6] = srcMACAddr[i];
+		
+		//Type = ARP
+		packet[12] = 0x08;
+		packet[13] = 0x06;
+		
+		//Hardware type = Ethernet
+		packet[14] = 0x00;
+		packet[15] = 0x01;
+		
+		//Protocol type = IPv4
+		packet[16] = 0x08;
+		packet[17] = 0x00;
+		
+		//Hardware size = 6
+		packet[18] = 0x06;
+		
+		//Protocol size = 4
+		packet[19] = 0x04;
+		
+		//Opcode = 1
+		packet[20] = 0x00;
+		packet[21] = 0x01;
+		
+		//src MAC address = srcMACAddr
+		for(int i=0;i<6;i++)
+			packet[i+22] = srcMACAddr[i];
+		
+		//src IP address = srcIPAddr
+		packet[28] = (byte) ((srcIPAddr>>24)&0xFF);
+		packet[29] = (byte) ((srcIPAddr>>16)&0xFF);
+		packet[30] = (byte) ((srcIPAddr>>8)&0xFF);
+		packet[31] = (byte) (srcIPAddr&0xFF);
+		
+		//des MAC address = 00:00:00:00:00:00
+		for(int i=32;i<38;i++)
+			packet[i] = 0x00;
+		
+		//des IP address = desIPAddr
+		packet[38] = (byte) ((desIPAddr>>24)&0xFF);
+		packet[39] = (byte) ((desIPAddr>>16)&0xFF);
+		packet[40] = (byte) ((desIPAddr>>8)&0xFF);
+		packet[41] = (byte) (desIPAddr&0xFF);
+		
+		//Padding
+		for(int i=42;i<60;i++)
+			packet[i] = 0x00;
+		
+		return packet;
 	}
 	
 	public byte[] generateARPreplyPacket(int srcIPAddr,byte[] srcMACAddr,int desIPAddr,byte[] desMACAddr) {//
@@ -48,7 +103,7 @@ public class ARP extends TimerTask{
 		return null;
 	}
 	
-	public void addARPtable(byte[] packet) {//
+	public void addARPtable(byte[] framePacket) {//
 		
 	}
 	
