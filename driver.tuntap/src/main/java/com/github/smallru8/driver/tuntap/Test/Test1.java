21398069a538461c4e9b59ccbb1165ce99335e12
smallru8 <30855604+smallru8@users.noreply.github.com>
2020-09-17 01:08:02 +0800

    no message

--- driver.tuntap/src/main/java/com/github/smallru8/driver/tuntap/Test/Test1.java
+++ driver.tuntap/src/main/java/com/github/smallru8/driver/tuntap/Test/Test1.java
@@ -1,11 +1,19 @@
 package com.github.smallru8.driver.tuntap.Test;
 
 import com.github.smallru8.driver.tuntap.TapDevice;
+import com.github.smallru8.driver.tuntap.TunTap;
 
 public class Test1 {
 
-	public static void main( String[] args ) {
-		TapDevice td = new TapDevice();
+	public static void main( String[] args ) throws InterruptedException {
+		TunTap tt = new TunTap();
+		tt.tuntap_up();
+		while(true) {
+			byte[] buff = tt.tuntap_read(1560);
+			if(buff!=null)
+				System.out.println(buff.length);
+			Thread.sleep(100);
+		}
 	}
 	
 }
